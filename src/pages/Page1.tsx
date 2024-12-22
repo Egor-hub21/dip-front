9ac35830394d59fb600676d584a14073f34cbe3f
Egor Kichigin <eek2077@outlook.com>
2024-12-22 19:52:54 +0700

    add Sidebar

---
+++ src/pages/Page1.tsx
@@ -0,0 +1,13 @@
+// src/pages/Page1.tsx
+import React from 'react'
+import { Box, Text } from '@chakra-ui/react'
+
+const Page1 = () => {
+  return (
+    <Box p={8}>
+      <Text fontSize="3xl">Welcome to Page 1</Text>
+    </Box>
+  )
+}
+
+export default Page1
---
+++ src/pages/Page2.tsx
@@ -0,0 +1,13 @@
+// src/pages/Page2.tsx
+import React from 'react'
+import { Box, Text } from '@chakra-ui/react'
+
+const Page2 = () => {
+  return (
+    <Box p={8}>
+      <Text fontSize="3xl">Welcome to Page 2</Text>
+    </Box>
+  )
+}
+
+export default Page2
